69b60df6c3f2730eb48238d2a4aa3c477852e91a
danci-silicone <danijel.klarin@gmail.com>
2020-04-09 12:27:44 +0200

    Updates

---
+++ precache-manifest.78fd923e7058b1d0489b34455242cbb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f5545fac692e509e313e91338b75d1",
+    "url": "/Korona/index.html"
+  },
+  {
+    "revision": "1dfa211d73a1381598ec",
+    "url": "/Korona/static/css/main.288aedd1.chunk.css"
+  },
+  {
+    "revision": "84e157577112ef565404",
+    "url": "/Korona/static/js/2.9e80e350.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Korona/static/js/2.9e80e350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dfa211d73a1381598ec",
+    "url": "/Korona/static/js/main.035d16bd.chunk.js"
+  },
+  {
+    "revision": "6777b1ba69d17d5767fb",
+    "url": "/Korona/static/js/runtime-main.7273ba27.js"
+  }
+]);
